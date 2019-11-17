6aa919144b5700e1f039655e90165b0ae31519c1
Антон Разумов <a.razumov@g.nsu.ru>
2019-11-17 16:10:55 +0700

    Add in dir search

--- java2/src/main/java/ru/nsu/fit/g16203/Main.java
+++ java2/src/main/java/ru/nsu/fit/g16203/Main.java
@@ -1,96 +1,75 @@
 package ru.nsu.fit.g16203;
 
-import ru.nsu.fit.g16203.data.DataGetter;
+import ru.nsu.fit.g16203.util.file.FileUtil;
 
 import java.io.File;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-//https://pastebin.com/x9TzKb
 public class Main {
 
-    private static final String MAN = "[--name fileName] [--data searchString] <folder|file>";
-    private static String searchString;
-    private static File startFile;
-    private static Predicate<File> fileFilter;
+    private static final String HELP = "[--name fileName] [--data stringToFind] <folder|file>";
+    private static final String NOT_ENOUGH_ERROR = "Not enough args";
+    private static final String WRONG_INPUT_ERROR = "Wrong input";
+    private static String stringToFind;
+    private static File file;
+    private static String fileNameToFind;
 
     public static void main(String[] args) {
 
         parseArgs(args);
 
-        List<File> files = getFileList(startFile, fileFilter);  //TODO
-
-        /*
-         * print file list
-         */
-        if (searchString == null) {
-            for (File f : files) {
-                System.out.println(f.getName());
+        if (file != null) {
+            if (stringToFind != null) {
+                //TODO
             }
-            System.exit(0);
+            if (fileNameToFind != null) {
+                FileUtil.getAllFiles(fileNameToFind, file);
+            }
+        } else {
+            printErrorAndTerminate(WRONG_INPUT_ERROR);
         }
 
-        List<DataGetter> dataGetters = new ArrayList<>(files.size());
-
-        for (File f : files) {
-
-        }
-
-        for (File file : files) {
-//            DataGetter dataGetter = new DataGetter(file, searchString);
-//            dataGetters.add(dataGetter);
-//            dataGetter.run();
-        }
-        System.out.println("String was found in files: ");
-//        dataGetters.stream()
-//                .filter(DataGetter::isFoundString)
-//                .forEach(dg -> System.out.println(dg.getFile()));
     }
 
     private static void parseArgs(String[] args) {
         if (args.length < 1) {
-            printErrorAndTerminate();
+            printErrorAndTerminate(NOT_ENOUGH_ERROR);
         }
 
         for (int i = 0; i < args.length; i++) {
             if (args[i].toLowerCase().equals("--name")) {
                 if (i + 1 == args.length) {
-                    printErrorAndTerminate();
+                    printErrorAndTerminate(NOT_ENOUGH_ERROR);
                 }
-                String fileName = args[i + 1];
-                fileFilter = f -> f.getName().equals(fileName);
+                fileNameToFind = args[i + 1];
                 i++;
             } else if (args[i].toLowerCase().equals("--data")) {
                 if (i + 1 == args.length) {
-                    printErrorAndTerminate();
+                    printErrorAndTerminate(NOT_ENOUGH_ERROR);
                 }
-                searchString = args[i + 1];
+                stringToFind = args[i + 1];
                 i++;
             } else if (args[i].toLowerCase().toLowerCase().equals("help")) {
                 printHelpAndTerminate();
                 break;
             } else {
-                startFile = Paths.get(args[i]).toFile();
+                try {
+                    file = Paths.get(args[i]).toFile();
+                } catch (InvalidPathException e) {
+                    printErrorAndTerminate(WRONG_INPUT_ERROR);
+                }
             }
-        }
-        if (startFile == null) {
-            printErrorAndTerminate();
         }
     }
 
-    private static void printErrorAndTerminate() {
-        System.err.println("No enough args");
+    private static void printErrorAndTerminate(String err) {
+        System.err.println(err);
         printHelpAndTerminate();
     }
 
     private static void printHelpAndTerminate() {
-        System.err.println("No enough args");
-        System.out.println(MAN);
+        System.out.println(HELP);
         System.exit(-1);
     }
 
--- java2/src/main/java/ru/nsu/fit/g16203/util/file/FileUtil.java
+++ java2/src/main/java/ru/nsu/fit/g16203/util/file/FileUtil.java
@@ -1,14 +1,15 @@
 package ru.nsu.fit.g16203.util.file;
 
-import java.io.*;
-import java.util.LinkedList;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 public class FileUtil {
 
     public static void getAllFiles(String str, File dir) {
-        Stack<File> dirs = new Stack<File>();
+        List<File> foundFiles = new ArrayList<>();
+        Stack<File> dirs = new Stack<>();
         dirs.push(dir);
         while (!dirs.isEmpty()) {
             File[] files = dirs.pop().listFiles();
@@ -17,50 +18,16 @@ public class FileUtil {
                     if (f.isDirectory()) {
                         dirs.push(f);
                     } else {
-                        search(str, f);
+                        if (f.getName().contains(str)) {
+                            foundFiles.add(f);
+                        }
                     }
                 }
             }
         }
-    }
-
-    static void search(String str, File f) {
-        final TwoWaySearch algorithm = new TwoWaySearch();
-        final List<Long> positions = new LinkedList<Long>();
-        final int length = str.length();
-        final byte[] needed = str.getBytes();
-        final long fileSize = f.length();
-        long currentPos = 0L;
-        BufferedInputStream reader;
-        try {
-            reader = new BufferedInputStream(new FileInputStream(file));
-        } catch (FileNotFoundException e2) {
-            System.out.println("File not found");
+        if (!dir.isDirectory() && dir.getName().contains(str)) {
+            foundFiles.add(dir);
         }
-        final byte[] array = new byte[1048576];
-        try {
-            while (currentPos < fileSize) {
-                int size = 0;
-                if (reader != null) {
-                    reader.mark(1048576);
-                    size = reader.read(array, 0, 1048576);
-                    reader.reset();
-                    reader.skip(size - length);
-                }
-                final List<Long> positionList = algorithm.indexOf(needed, array, size);
-                for (final Long val : positionList) {
-                    positions.add(val + currentPos);
-                }
-                currentPos += size;
-                if (currentPos < fileSize - 1L) {
-                    currentPos -= length;
-                }
-            }
-            positions.forEach(pos -> System.out.println(f.getName() + ' ' + pos));
-        } catch (IOException e) {
-            System.out.println(f.getName());
-            System.out.println(e.getMessage());
-        }
+        foundFiles.forEach(System.out::println);
     }
-
 }
